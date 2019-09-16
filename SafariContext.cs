4c6d2be6bc435421ee08483092c8efd33bb53823
Jessica Evans <jme@mail.usf.edu>
2019-09-16 17:12:08 -0400

    project created

---
+++ SafariContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Safari
+{
+    public partial class SafariContext : DbContext
+    {
+        public SafariContext()
+        {
+        }
+
+        public SafariContext(DbContextOptions<SafariContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseNpgsql("server=localhost;database=SafariVacation");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+        }
+    }
+}
