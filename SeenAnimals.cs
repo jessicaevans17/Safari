95c6726f2c5b19b8b3f126167479298a360e17b5
Jessica Evans <jme@mail.usf.edu>
2019-09-16 22:39:40 -0400

    added last seen time column

--- SeenAnimals.cs
+++ SeenAnimals.cs
@@ -7,5 +7,6 @@ namespace Safari
     public string Species { get; set; }
     public int CountOfTimesSeen { get; set; }
     public string LocationOfLastSeen { get; set; }
+    public DateTime LastSeenTime { get; set; }
   }
 }
